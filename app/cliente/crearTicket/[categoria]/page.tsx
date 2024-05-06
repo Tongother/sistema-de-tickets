887453df87f48578ed9353d4b66e3d26db44d59c
Pollicida <cristiancfack133@gmail.com>
2024-05-05 19:45:29 -0600

    Fixing client page

---
+++ app/cliente/crearTicket/[categoria]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FormCategoria = (categoria: any) => {
+  const categoriaProblematica = categoria;
+  console.log(categoria);
+  //const idCliente = ;
+  return(
+    <div>
+      <h1>dasdasda</h1>
+    </div>
+  )
+}
+export default FormCategoria;
