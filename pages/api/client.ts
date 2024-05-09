9bc0557feefd4766ce0d333c8877a223d4588580
Gunther <nettelgunther@outlook.es>
2024-05-09 11:26:24 -0600

    Testing

--- pages/api/client.ts
+++ pages/api/client.ts
@@ -9,8 +9,8 @@ export default async function GET(
     const client = await db.connect();
   try {
     await client.sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255));`;
-    // const names = ["Fiona", "Lucy"]
-    // await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
+    const names = ["Fiona", "Lucy"]
+    await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
   } catch (error) {
     return res.status(500).json({ error });
   }
