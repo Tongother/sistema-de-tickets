8e1ac12071dfa4d9554ffa2021df85c9deb125de
Pollicida <cristiancfack133@gmail.com>
2024-05-12 19:18:40 -0600

    Styled progressBar

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,60 +1,72 @@
 
 import '@/styles/progressBarStyle.css';
-import { useEffect } from 'react';
+import {useRef} from 'react';
+import Image from 'next/image';
+import recibido from '@/public/recibido.svg';
+import pruebas from '@/public/pruebas.svg';
+import searchT from '@/public/search-ticket.svg';
+import check from '@/public/check.svg';
+import checkCircle from '@/public/check-circle.svg';
+import hammer from '@/public/jackhammer.svg';
+
 export default function ProgressBar(){
-    useEffect(()=>{
-        const one = document.querySelector(".one") as HTMLElement;
-        const two = document.querySelector(".two") as HTMLElement;
-        const three = document.querySelector(".three") as HTMLElement;
-        const four = document.querySelector(".four") as HTMLElement;
-        const five = document.querySelector(".five") as HTMLElement;
-    
-        const handleClickOne = () => {
-            one.classList.add("active");
-            two.classList.remove("active");
-            three.classList.remove("active");
-            four.classList.remove("active");
-            five.classList.remove("active");
-        };
-    
-        const handleClickTwo = () => {
-            one.classList.add("active");
-            two.classList.add("active");
-            three.classList.remove("active");
-            four.classList.remove("active");
-            five.classList.remove("active");
-        };
-    
-        const handleClickThree = () => {
-            one.classList.add("active");
-            two.classList.add("active");
-            three.classList.add("active");
-            four.classList.remove("active");
-            five.classList.remove("active");
-        };
-    
-        const handleClickFour = () => {
-            one.classList.add("active");
-            two.classList.add("active");
-            three.classList.add("active");
-            four.classList.add("active");
-            five.classList.remove("active");
-        };
-    
-        const handleClickFive = () => {
-            one.classList.add("active");
-            two.classList.add("active");
-            three.classList.add("active");
-            four.classList.add("active");
-            five.classList.add("active");
-        };
-    
-        one.onclick = handleClickOne;
-        two.onclick = handleClickTwo;
-        three.onclick = handleClickThree;
-        four.onclick = handleClickFour;
-        five.onclick = handleClickFive;
-    },[]);
+
+    const one = useRef<HTMLDivElement>(null);
+    const two = useRef<HTMLDivElement>(null);
+    const three = useRef<HTMLDivElement>(null);
+    const four = useRef<HTMLDivElement>(null);
+    const five = useRef<HTMLDivElement>(null);
+
+
+    const handleClickOne = () => {
+        if(one.current != null && two.current != null && three.current != null && four.current != null && five.current != null){
+            one.current.classList.add("active");
+            two.current.classList.remove("active");
+            three.current.classList.remove("active");
+            four.current.classList.remove("active");
+            five.current.classList.remove("active");
+        }
+    };
+
+    const handleClickTwo = () => {
+        if(one.current != null && two.current != null && three.current != null && four.current != null && five.current != null){
+            one.current.classList.add("active");
+            two.current.classList.add("active");
+            three.current.classList.remove("active");
+            four.current.classList.remove("active");
+            five.current.classList.remove("active");
+        }
+    };
+
+    const handleClickThree = () => {
+        if(one.current != null && two.current != null && three.current != null && four.current != null && five.current != null){
+            one.current.classList.add("active");
+            two.current.classList.add("active");
+            three.current.classList.add("active");
+            four.current.classList.remove("active");
+            five.current.classList.remove("active");
+        }
+    };
+
+    const handleClickFour = () => {
+        if(one.current != null && two.current != null && three.current != null && four.current != null && five.current != null){
+            one.current.classList.add("active");
+            two.current.classList.add("active");
+            three.current.classList.add("active");
+            four.current.classList.add("active");
+            five.current.classList.remove("active");
+        }
+    };
+
+    const handleClickFive = () => {
+        if(one.current != null && two.current != null && three.current != null && four.current != null && five.current != null){
+            one.current.classList.add("active");
+            two.current.classList.add("active");
+            three.current.classList.add("active");
+            four.current.classList.add("active");
+            five.current.classList.add("active");
+        }
+    };
 
     return(
         <div className="main">
@@ -66,40 +78,39 @@ export default function ProgressBar(){
 
             <ul>
                 <li>
-                    <i className="icon uil uil-capture"></i>
-                    <div className="progress one">
+                    <Image alt="" src={recibido.src} width={50} height={50}></Image>
+                    <div className="progress1" ref={one} onClick={handleClickOne}>
                         <p>1</p>
-                        <i className="uil uil-check"></i>
                     </div>
                     <p className="text">Recibido</p>
                 </li>
                 <li>
-                    <i className="icon uil uil-clipboard-notes"></i>
-                    <div className="progress two">
+                    <Image alt="" src={searchT.src} width={50} height={50}></Image>
+                    <div className="progress" ref={two} onClick={handleClickTwo}>
                         <p>2</p>
                         <i className="uil uil-check"></i>
                     </div>
                     <p className="text">Analizado</p>
                 </li>
                 <li>
-                    <i className="icon uil uil-credit-card"></i>
-                    <div className="progress three">
+                    <Image alt="" src={hammer.src} width={50} height={50}></Image>
+                    <div className="progress" ref={three} onClick={handleClickThree}>
                         <p>3</p>
                         <i className="uil uil-check"></i>
                     </div>
                     <p className="text">En proceso de resolución</p>
                 </li>
                 <li>
-                    <i className="icon uil uil-exchange"></i>
-                    <div className="progress four">
+                    <Image alt="" src={pruebas.src} width={50} height={50}></Image>
+                    <div className="progress" ref={four} onClick={handleClickFour}>
                         <p>4</p>
                         <i className="uil uil-check"></i>
                     </div>
                     <p className="text">Pruebas</p>
                 </li>
                 <li>
-                    <i className="icon uil uil-map-marker"></i>
-                    <div className="progress five">
+                    <Image alt="" src={checkCircle.src} width={50} height={50}></Image>
+                    <div className="progress" ref={five} onClick={handleClickFive}>
                         <p>5</p>
                         <i className="uil uil-check"></i>
                     </div>
